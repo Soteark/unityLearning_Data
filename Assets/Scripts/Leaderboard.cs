74c2f826ea7b95ebdf88888e1bc8c0a1c388d41c
Cy <slash.young@gmail.com>
2023-07-09 10:23:20 -0700

    Added Menu Scene
    created new menu scene

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    public string playerName;
+    public int highScore = 0;
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,6 +11,7 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text bestScoreText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -72,5 +73,8 @@ public class MainManager : MonoBehaviour
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+
+        // Update Best Score 
+        
     }
 }
