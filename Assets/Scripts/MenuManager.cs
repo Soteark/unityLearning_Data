d0e748de7edbaf15e386d6f2d492355f0b9048b1
Cy <slash.young@gmail.com>
2023-07-09 10:41:05 -0700

    Added Exit Functionality.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuManager : MonoBehaviour
 {
     public void StartClick(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
     }
 }
